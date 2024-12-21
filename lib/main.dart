544d15db791d340d5b4c0a3225edaf10675d84e6
Mouhamed Diallo <mouhameddiallo@mbp-de-mouhamed.home>
2024-12-21 19:39:16 +0000

    first-commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const HomePage());
+}
+
+class HomePage extends StatelessWidget{
+
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context){
+    return const Center(
+      child: Text(
+        "Home Page Test",
+        textDirection: TextDirection.ltr,
+      ),
+    );
+  }
+}
